fe78a3d73423888504099adf06c891833af55c81
Alessandro <alessandro.galasso@gmail.com>
2020-04-11 15:25:00 +0200

    Added some TODO notes.

--- pusher/main.go
+++ pusher/main.go
@@ -3,6 +3,12 @@
 //
 // TODO 2: add a flag to switch between sequential implementation (that guarantees
 // rows order) and parallel implementation (no order guaranteed).
+// Move the implementation in a sender package with:
+// 		func Send(b []byte)
+// 		func WaitForCompletion()
+//		func State() []error
+//		MaxConcurrency uint
+//		TargetURL string
 package main
 
 import (
