bf9af4b243d4bbf81dd72b778843885ef6ddebb0
Alessandro <alessandro.galasso@gmail.com>
2020-04-14 10:29:38 +0200

    Added first version of ingestor service.

--- matschmazia/ingestor/main.go
+++ matschmazia/ingestor/main.go
@@ -1,6 +1,13 @@
 // Consumer is a REST service that waits for sensors data as flat JSON and store them in an InfluxDB database.
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 // More information on: https://browser.lter.eurac.edu/p/info.md
 const (
 	time              = iota // Date/time of measurement (UTC +1).
@@ -22,4 +29,30 @@ const (
 )
 
 func main() {
+	http.HandleFunc("/sensordata", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	//fmt.Scanln() // wait for Enter Key
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s %s\n", r.Method, r.URL, r.Proto)
+
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+
+	body, err := ioutil.ReadAll(r.Body) // Not safe implementation: just for testing purpose.
+	if err == nil {
+		fmt.Fprintf(w, "Body = %q\n", body)
+		log.Printf("Body = %s", body)
+	}
 }
