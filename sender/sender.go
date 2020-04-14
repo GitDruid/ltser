78dd340cb1aa944465091c14292619b6c435b14e
Alessandro <alessandro.galasso@gmail.com>
2020-04-14 12:05:10 +0200

    Added todo note.

--- sender/sender.go
+++ sender/sender.go
@@ -1,13 +1,12 @@
 // Package sender provide an interface to send json data to a target.
 //
-// TODO: add a flag to switch between sequential implementation (that guarantees
-// rows order) and parallel implementation (no order guaranteed).
-// Move the implementation in a sender package with:
-// 		func Send(b []byte)
-// 		func WaitForCompletion()
-//		func State() []error
-//		MaxConcurrency uint
-//		TargetURL string
+// TODO: add a MaxConcurrency field (default 1) to switch between sequential implementation
+// (that guarantees order) and parallel implementation (no order guaranteed).
+// Add following methods:
+// 		func Send(b []byte)			starts a goroutine to send data
+// 		func WaitForCompletion()	waits for all running goroutines to complete
+//		func State() []error		returns a list of encountered errors
+//		MaxConcurrency uint			specifies how many goroutine can be called concurrently
 package sender // import "goex/ltser/sender"
 
 // A Sender send json objects to a target.
